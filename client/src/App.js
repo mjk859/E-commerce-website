030a5d80d172c25cdfc1cbe769d57bce9f1bdfe9
Jaseel <muhammedjaseelk10@gmail.com>
2025-01-08 19:14:52 +0530

    React import in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme } from "./utils/Themes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
